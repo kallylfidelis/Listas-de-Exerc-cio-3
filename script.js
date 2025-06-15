cb868673ec2e074125f147dc685b97d56729cd39
kallylfidelis <kallyl71107@edu.unipar.br>
2025-06-15 19:34:09 -0300

    ex 11

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-var dias = document.querySelector("#dias");
+var salario = document.querySelector("#salario");
 var btCalcular = document.querySelector("#btCalcular");
 var h3Resultado = document.querySelector("#h3Resultado");
 
-function calcularTempo() {
-    var anos = Math.floor(Number(dias.value) / 360);
-    var meses = Math.floor((Number(dias.value) % 360) / 30);
-    var diasRestantes = Number(dias.value) % 30;
-    
-    h3Resultado.innerHTML = `Tempo de trabalho: ${anos} anos, ${meses} meses e ${diasRestantes} dias.`;
+function calcularSalario() {
+    var aumento = Number(salario.value) * 0.15;
+    var salarioComAumento = Number(salario.value) + aumento;
+    var imposto = salarioComAumento * 0.08;
+    var salarioFinal = salarioComAumento - imposto;
+
+    h3Resultado.innerHTML = `
+        Salário Inicial: R$ ${Number(salario.value).toFixed(2)}<br>
+        Salário com Aumento: R$ ${salarioComAumento.toFixed(2)}<br>
+        Salário Final: R$ ${salarioFinal.toFixed(2)}
+    `;
 }
 
 btCalcular.onclick = function() {
-    calcularTempo();
+    calcularSalario();
 }
