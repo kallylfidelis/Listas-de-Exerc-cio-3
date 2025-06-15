e480a757f98fa1b3983476ecb7aed3bfe381ba88
kallylfidelis <kallyl71107@edu.unipar.br>
2025-06-15 19:33:35 -0300

    ex 9

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
-var quantidadePequena = document.querySelector("#quantidadePequena");
-var quantidadeMedia = document.querySelector("#quantidadeMedia");
-var quantidadeGrande = document.querySelector("#quantidadeGrande");
+var x1 = document.querySelector("#x1");
+var y1 = document.querySelector("#y1");
+var x2 = document.querySelector("#x2");
+var y2 = document.querySelector("#y2");
 var btCalcular = document.querySelector("#btCalcular");
 var h3Resultado = document.querySelector("#h3Resultado");
 
-function calcularTotal() {
-    var total = (Number(quantidadePequena.value) * 10) +
-                (Number(quantidadeMedia.value) * 12) +
-                (Number(quantidadeGrande.value) * 15);
-    h3Resultado.innerHTML = `Total arrecadado: R$ ${total.toFixed(2)}`;
+function calcularDistancia() {
+    var distancia = Math.sqrt(Math.pow(Number(x2.value) - Number(x1.value), 2) + Math.pow(Number(y2.value) - Number(y1.value), 2));
+    h3Resultado.innerHTML = `A distância entre os pontos é ${distancia.toFixed(2)} unidades.`;
 }
 
 btCalcular.onclick = function() {
-    calcularTotal();
+    calcularDistancia();
 }
