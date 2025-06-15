595d6bba65ac0f927c7f6b5c2ee2bae85596c7fd
kallylfidelis <kallyl71107@edu.unipar.br>
2025-06-15 19:32:01 -0300

    ex 3

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-var quantidadeCavalos = document.querySelector("#quantidadeCavalos");
+var quantidadePao = document.querySelector("#quantidadePao");
+var quantidadeBroa = document.querySelector("#quantidadeBroa");
 var btCalcular = document.querySelector("#btCalcular");
 var h3Resultado = document.querySelector("#h3Resultado");
 
-function calcularFerraduras() {
-    var ferraduras = Number(quantidadeCavalos.value) * 4;
-    h3Resultado.innerHTML = `Serão necessárias ${ferraduras} ferraduras.`;
+function calcularVendas() {
+    var totalPao = Number(quantidadePao.value) * 0.12;
+    var totalBroa = Number(quantidadeBroa.value) * 1.50;
+    var totalArrecadado = totalPao + totalBroa;
+    var poupanca = totalArrecadado * 0.10;
+    
+    h3Resultado.innerHTML = `Total arrecadado: R$ ${totalArrecadado.toFixed(2)}<br>Guardar na poupança: R$ ${poupanca.toFixed(2)}`;
 }
 
 btCalcular.onclick = function() {
-    calcularFerraduras();
+    calcularVendas();
 }
