6e011cc2651ec59a555a9762340274825449ec93
kallylfidelis <kallyl71107@edu.unipar.br>
2025-06-15 19:34:25 -0300

    ex 12

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-var salario = document.querySelector("#salario");
+var numero = document.querySelector("#numero");
 var btCalcular = document.querySelector("#btCalcular");
 var h3Resultado = document.querySelector("#h3Resultado");
 
-function calcularSalario() {
-    var aumento = Number(salario.value) * 0.15;
-    var salarioComAumento = Number(salario.value) + aumento;
-    var imposto = salarioComAumento * 0.08;
-    var salarioFinal = salarioComAumento - imposto;
+function calcularCentenas() {
+    var centena = Math.floor(Number(numero.value) / 100);
+    var dezena = Math.floor((Number(numero.value) % 100) / 10);
+    var unidade = Number(numero.value) % 10;
 
-    h3Resultado.innerHTML = `
-        Salário Inicial: R$ ${Number(salario.value).toFixed(2)}<br>
-        Salário com Aumento: R$ ${salarioComAumento.toFixed(2)}<br>
-        Salário Final: R$ ${salarioFinal.toFixed(2)}
-    `;
+    h3Resultado.innerHTML = `CENTENA = ${centena} <br> DEZENA = ${dezena} <br> UNIDADE = ${unidade}`;
 }
 
 btCalcular.onclick = function() {
-    calcularSalario();
+    calcularCentenas();
 }
