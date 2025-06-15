2d40f663c175f53e6430adb6812c05f7e623819a
kallylfidelis <kallyl71107@edu.unipar.br>
2025-06-15 19:34:49 -0300

    ex 13

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var numero = document.querySelector("#numero");
-var btCalcular = document.querySelector("#btCalcular");
-var h3Resultado = document.querySelector("#h3Resultado");
+const inputRaio = document.querySelector("#raio");
+const btCalcular = document.querySelector("#btCalcular");
+const resultado = document.querySelector("#h3Resultado");
 
-function calcularCentenas() {
-    var centena = Math.floor(Number(numero.value) / 100);
-    var dezena = Math.floor((Number(numero.value) % 100) / 10);
-    var unidade = Number(numero.value) % 10;
-
-    h3Resultado.innerHTML = `CENTENA = ${centena} <br> DEZENA = ${dezena} <br> UNIDADE = ${unidade}`;
+function calcularAreaPizza() {
+  const raio = Number(inputRaio.value);
+  if (raio <= 0 || isNaN(raio)) {
+    resultado.textContent = "Por favor, insira um valor válido para o raio.";
+    return;
+  }
+  const pi = 3.14;
+  const area = pi * raio * raio;
+  resultado.textContent = `A área da pizza é ${area.toFixed(2)} cm².`;
 }
 
-btCalcular.onclick = function() {
-    calcularCentenas();
-}
+btCalcular.onclick = calcularAreaPizza;
