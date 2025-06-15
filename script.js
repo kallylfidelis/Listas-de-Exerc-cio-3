1614b68e9321a321416a7918356ba12b8e9bba27
kallylfidelis <kallyl71107@edu.unipar.br>
2025-06-15 19:33:52 -0300

    ex 10

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var x1 = document.querySelector("#x1");
-var y1 = document.querySelector("#y1");
-var x2 = document.querySelector("#x2");
-var y2 = document.querySelector("#y2");
+var dias = document.querySelector("#dias");
 var btCalcular = document.querySelector("#btCalcular");
 var h3Resultado = document.querySelector("#h3Resultado");
 
-function calcularDistancia() {
-    var distancia = Math.sqrt(Math.pow(Number(x2.value) - Number(x1.value), 2) + Math.pow(Number(y2.value) - Number(y1.value), 2));
-    h3Resultado.innerHTML = `A distância entre os pontos é ${distancia.toFixed(2)} unidades.`;
+function calcularTempo() {
+    var anos = Math.floor(Number(dias.value) / 360);
+    var meses = Math.floor((Number(dias.value) % 360) / 30);
+    var diasRestantes = Number(dias.value) % 30;
+    
+    h3Resultado.innerHTML = `Tempo de trabalho: ${anos} anos, ${meses} meses e ${diasRestantes} dias.`;
 }
 
 btCalcular.onclick = function() {
-    calcularDistancia();
+    calcularTempo();
 }
